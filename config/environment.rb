780310bb667c7472d665a869542b227400383de2
Neil Marion dela Cruz <nmfdelacruz@gmail.com>
2013-10-28 21:45:02 +0800

    Run by staging

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,5 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
-
+RAILS_ENV="staging"
 # Initialize the Rails application.
 Countdown2014::Application.initialize!
