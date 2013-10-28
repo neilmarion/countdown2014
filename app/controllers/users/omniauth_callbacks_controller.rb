80757d5bded4c357c40fa2451f0ab3ec683bd0f8
Neil Marion dela Cruz <nmfdelacruz@gmail.com>
2013-10-28 16:56:56 +0800

    Fixed alert

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -4,7 +4,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     user = User.from_omniauth(request.env["omniauth.auth"])
 
     if user.persisted?
-      flash.notice = "Signed in!"
+      flash.notice = "Thanks for sharing! Wait for a while until the photo is uploaded to your Facebook profile"
       sign_in_and_redirect user
     else
       session["devise.user_attributes"] = user.attributes
