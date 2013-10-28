60f30525e8505dad7a1da69c0162106def6c7219
Neil Marion dela Cruz <nmfdelacruz@gmail.com>
2013-10-28 10:20:09 +0800

    First.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Countdown2014::Application.config.secret_key_base = 'd670270243c831a5b734bd9c8d448f25d9016df5247aa271422f6d63bc055c95af3d3a0a34d7f1c0919610fd69577af88b904932c158a0e0f54123cb6c7427bd'
