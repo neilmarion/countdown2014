6f941bb31bc9404c69b514700225bdc58e250626
Neil Marion dela Cruz <nmfdelacruz@gmail.com>
2013-10-28 12:24:29 +0800

    Fixed tests

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,12 +1,14 @@
 require 'spec_helper'
 
 describe HomeController do
+  include_context "common controller stuff" 
 
   describe "GET 'index'" do
     it "returns http success" do
+      user = FactoryGirl.create(:user_facebook)
+      sign_in user 
       get 'index'
       response.should be_success
     end
   end
-
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,14 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
 
+require File.expand_path("../../config/environment", __FILE__)
+require 'rspec/rails'
+require 'rspec/autorun'
+require 'factory_girl'
+require 'webmock/rspec'
+FactoryGirl.find_definitions
+
+
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
@@ -22,6 +30,8 @@ RSpec.configure do |config|
   # config.mock_with :rr
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
+  config.include Devise::TestHelpers, :type => :controller
+
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
