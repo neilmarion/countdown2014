69a90ac92c9c727623c70e3e2be579a11bcaa1bd
Neil Marion dela Cruz <nmfdelacruz@gmail.com>
2013-11-01 13:26:03 +0800

    changed redirect_uri

--- app/controllers/facebook_controller.rb
+++ app/controllers/facebook_controller.rb
@@ -5,7 +5,7 @@ class FacebookController < ApplicationController
   after_filter :allow_iframe
 
   def canvas 
-    redirect_to "https://www.facebook.com/dialog/oauth?client_id=#{FB['key']}&redirect_uri=https://apps.facebook.com/inccountdowncent"
+    redirect_to "https://www.facebook.com/dialog/oauth?client_id=#{FB['key']}&redirect_uri=https://www.countdown2014.org"
   end
 
   def allow_iframe
